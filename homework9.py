4c1e4929c19018d00a12df3d329067603b13df48
Lytsyk-Diana <Diana95073@gmail.com>
2024-03-01 22:13:40 +0200

    homework 9

---
+++ homework9.py
@@ -0,0 +1,23 @@
+my_str = input("enter: ")
+my_list = my_str.split()
+print(my_list)
+
+my_str1 = input("enter: ")
+my_str2 = input("enter: ")
+my_list1=list(my_str1)
+my_list2=list(my_str2)
+print(my_list1,my_list2)
+
+str1 =input('')
+str2 =input('')
+result=str1<str2
+print(result)
+
+my_str=input("Введіть строку: ")
+
+for i in my_str:
+    print(i)
+
+my_str=input ('enter: ').lower ()
+print (my_str. count ('b')+my_str.count ('c')+my_str.count ('d')+my_str.count('f')+my_str.count('g'),my_str. count ('h')+my_str.count ('j')+my_str.count ('k')+my_str.count('l')+my_str.count('m')+my_str. count ('n')+my_str.count ('p')+my_str.count ('q')+my_str.count('r')+my_str.count('s')+my_str. count ('t')+my_str.count('v')+my_str.count ('w')+my_str.count('x')+my_str.count('y')+my_str.count('z'))
+
